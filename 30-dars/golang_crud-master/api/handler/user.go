f4ffca57f968fc760409f2b6f998c36d953147ad
shakhboznorbekov <shaxboznorbekov450@gmail.com>
2022-12-12 23:53:34 +0500

    swagger

---
+++ 30-dars/golang_crud-master/api/handler/user.go
@@ -0,0 +1,181 @@
+package handler
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/asadbekGo/golang_crud/models"
+	"github.com/asadbekGo/golang_crud/storage"
+)
+
+// CreateUser godoc
+// @ID create_user
+// @Router /user [POST]
+// @Summary Create User
+// @Description Create User
+// @Tags User
+// @Accept json
+// @Produce json
+// @Param user body models.User true "CreateUserRequestBody"
+// @Success 201 {object} models.User "GetUserBody"
+// @Response 400 {object} string "Invalid Argument"
+// @Failure 500 {object} string "Server Error"
+func (h *HandlerV1) Create(c *gin.Context) {
+
+	var (
+		user models.User
+	)
+
+	err := c.ShouldBindJSON(&user)
+	if err != nil {
+		log.Printf("error whiling create: %v\n", err)
+		c.JSON(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	id, err := storage.Create(h.db, user)
+	if err != nil {
+		log.Printf("error whiling create: %v\n", err)
+		c.JSON(http.StatusInternalServerError, errors.New("error whiling create").Error())
+		return
+	}
+
+	userId, err := storage.GetById(h.db, id)
+	if err != nil {
+		log.Printf("error whiling get by id: %v\n", err)
+		c.JSON(http.StatusInternalServerError, errors.New("error whiling get by id").Error())
+		return
+	}
+
+	c.JSON(http.StatusCreated, userId)
+}
+
+// GetByIdUser godoc
+// @ID get_by_id_user
+// @Router /user/{id} [GET]
+// @Summary Get By Id User
+// @Description Get By Id User
+// @Tags User
+// @Accept json
+// @Produce json
+// @Param id path string true "id"
+// @Success 200 {object} models.User "GetUserBody"
+// @Response 400 {object} string "Invalid Argument"
+// @Failure 500 {object} string "Server Error"
+func (h *HandlerV1) GetById(c *gin.Context) {
+
+	id := c.Param("id")
+
+	user, err := storage.GetById(h.db, id)
+	if err != nil {
+		log.Printf("error whiling get by id: %v\n", err)
+		c.JSON(http.StatusInternalServerError, errors.New("error whiling get by id").Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, user)
+}
+
+func (h *HandlerV1) GetList(c *gin.Context) {
+
+	users, err := storage.GetList(h.db)
+	if err != nil {
+		log.Printf("error whiling get list: %v", err)
+		c.JSON(http.StatusInternalServerError, errors.New("error whiling get list").Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, users)
+}
+
+func (h *HandlerV1) Update(c *gin.Context) {
+
+	var (
+		user models.User
+	)
+
+	err := c.ShouldBindJSON(&user)
+	if err != nil {
+		log.Printf("error whiling update: %v\n", err)
+		c.JSON(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	rowsAffected, err := storage.Update(h.db, user)
+	if err != nil {
+		log.Printf("error whiling update: %v", err)
+		c.JSON(http.StatusInternalServerError, errors.New("error whiling update").Error())
+		return
+	}
+
+	fmt.Println(rowsAffected)
+
+	if rowsAffected == 0 {
+		log.Printf("error whiling update rows affected: %v", err)
+		c.JSON(http.StatusInternalServerError, errors.New("error whiling update rows affected").Error())
+		return
+	}
+
+	resp, err := storage.GetById(h.db, user.Id)
+	if err != nil {
+		log.Printf("error whiling get by id: %v\n", err)
+		c.JSON(http.StatusInternalServerError, errors.New("error whiling get by id").Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, resp)
+}
+
+func (h *HandlerV1) Patch(c *gin.Context) {
+
+	var (
+		user models.UserData
+	)
+
+	err := c.ShouldBindJSON(&user)
+	if err != nil {
+		log.Printf("error whiling patch: %v\n", err)
+		c.JSON(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	rowsAffected, err := storage.Patch(h.db, user)
+	if err != nil {
+		log.Printf("error whiling patch: %v", err)
+		c.JSON(http.StatusInternalServerError, errors.New("error whiling patch").Error())
+		return
+	}
+
+	if rowsAffected == 0 {
+		log.Printf("error whiling update rows affected: %v", err)
+		c.JSON(http.StatusInternalServerError, errors.New("error whiling patch rows affected").Error())
+		return
+	}
+
+	resp, err := storage.GetById(h.db, user.Id)
+	if err != nil {
+		log.Printf("error whiling get by id: %v\n", err)
+		c.JSON(http.StatusInternalServerError, errors.New("error whiling get by id").Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, resp)
+}
+
+func (h *HandlerV1) Delete(c *gin.Context) {
+
+	id := c.Param("id")
+
+	err := storage.Delete(h.db, id)
+	if err != nil {
+		log.Printf("error whiling delete: %v", err)
+		c.JSON(http.StatusInternalServerError, errors.New("error whiling delete").Error())
+		return
+	}
+
+	c.JSON(http.StatusNoContent, nil)
+}
